13ff44d4f5d54c42ad864b748f449097a60059a8
Gerly Kaytukov <gerlyecc@yahoo.com>
2022-10-06 14:38:19 -0400

    added minus

--- functions.js
+++ functions.js
@@ -4,7 +4,11 @@
 //}
     
     const addTwo = require("./external.js")
+    const subtract = require("./minus.js")
 
-    
-    const sum = addTwo(3,4)// addTwo is a function//the 3 is the first parameter 4 is the second parameter
-    console.log(sum)
+
+    //const sum = addTwo(3,4)// addTwo is a function//the 3 is the first parameter 4 is the second parameter
+    //console.log(sum)
+
+    const total = subtract(10,5)
+    console.log(total)
